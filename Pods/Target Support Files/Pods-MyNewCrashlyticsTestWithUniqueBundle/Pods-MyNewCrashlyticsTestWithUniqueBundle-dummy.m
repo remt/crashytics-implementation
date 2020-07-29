a59ea530d905d10ea295528ed024ed933bc2cabc
Georgy Rybtsov <omoloko.develop@gmail.com>
2020-07-29 12:46:05 +0300

    Firebase SDK installation

---
+++ Pods/Target Support Files/Pods-MyNewCrashlyticsTestWithUniqueBundle/Pods-MyNewCrashlyticsTestWithUniqueBundle-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyNewCrashlyticsTestWithUniqueBundle : NSObject
+@end
+@implementation PodsDummy_Pods_MyNewCrashlyticsTestWithUniqueBundle
+@end
